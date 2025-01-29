8405e07d285b9482e014e69c8398da16de29ba62
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2025-01-29 12:55:36 +0530

    icon added at the url

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Kanit } from "next/font/google";
 import "./globals.css";
-// import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { NavbarDemo } from "@/components/Navbar";
 import { SpeedInsights } from '@vercel/speed-insights/next';
@@ -15,13 +14,28 @@ const kanit = Kanit({
 export const metadata = {
   title: "Supreme Infrastructure Company",
   description: "supreme infrastructure company",
+  icons: {
+    icon: [
+      {
+        url: '/images/home/logo.png',
+        href: '/images/home/logo.png',
+      },
+    ],
+    // You can also add Apple touch icon if needed
+    apple: [
+      {
+        url: '/images/home/logo.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={kanit.className}>
-        {/* <Header/> */}
         <NavbarDemo />
         {children}
         <SpeedInsights/>
