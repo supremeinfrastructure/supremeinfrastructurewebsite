91dec57bbb728b20e9935cc7202ed4b944db0c46
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2025-02-04 18:23:48 +0530

    new code

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -275,6 +275,7 @@ export default function Contact() {
                   )}
                 </div>
               </div>
+              
               <div>
                 <input
                   type='tel'
