bfb765074b611dbb0d2cdebbc006c0d996a06eb1
AkshayKalamkarr <kalamkarakshay2001@gmail.com>
2025-03-18 10:07:30 +0530

    button color update

--- app/about/architecturalDesigns/page.jsx
+++ app/about/architecturalDesigns/page.jsx
@@ -62,7 +62,7 @@ export default function Home() {
             <p className="text-gray-700 text-center md:my-6">We're Supreme's Interior Construction Specialists, and you can call us ICS. We're focused on specializations like transformation of commercial spaces, tenant improvements, and more.</p>
           </motion.div>
           <motion.div
-            className="bg-gradient-to-r from-amber-700 to-amber-600 text-white p-6"
+            className="bg-gradient-to-r from-amber-600 to-amber-600 text-white p-6"
           // initial={{ x: 50, opacity: 0 }}
           // whileInView={{ x: 0, opacity: 1 }}
           // transition={{ duration: 0.8, delay: 0.4 }}
@@ -126,7 +126,7 @@ export default function Home() {
             <div className="mt-4 flex space-x-4 justify-center">
               <motion.a
                 href="/contact"
-                className="bg-gradient-to-r from-amber-700 to-amber-600 text-white px-4 py-2"
+                className="bg-gradient-to-r from-amber-600 to-amber-600 text-white px-4 py-2"
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
               >
@@ -200,7 +200,7 @@ export default function Home() {
               // whileHover={{ scale: 1.05 }}
               // whileTap={{ scale: 0.95 }}
               >
-                <Link href="/project/commercialprojects" className="mt-4 inline-block bg-gradient-to-r from-amber-700 to-amber-600 text-white px-4 mx-24 py-2 md:mx-64">View Projects</Link>
+                <Link href="/project/commercialprojects" className="mt-4 inline-block bg-gradient-to-r from-amber-600 to-amber-600 text-white px-4 mx-24 py-2 md:mx-64">View Projects</Link>
               </motion.div>
             </div>
           </motion.div>
@@ -236,7 +236,7 @@ export default function Home() {
           </motion.p>
           <motion.a
             href="/contact/career"
-            className="mt-4 inline-block bg-gradient-to-r from-amber-700 to-amber-600 text-white px-6 py-3"
+            className="mt-4 inline-block bg-gradient-to-r from-amber-600 to-amber-600 text-white px-6 py-3"
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
             initial={{ opacity: 0 }}
