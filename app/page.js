7a440570faa6113a40f6ee15036976e2a4339f1d
AkshayKalamkarr <kalamkarakshay2001@gmail.com>
2025-04-23 09:53:40 +0530

    new code

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import HeroSection from "@/components/HeroSection";
 import Portfolio from "@/components/Portfolio";
 import SupremeQuote from "@/components/SupremeQuote";
 
+
 export default function Home() {
   return (
     <>
