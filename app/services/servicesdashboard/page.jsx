121a9e33831cf95d6e3954743be2ecf811bc50b0
AkshayKalamkarr <kalamkarakshay2001@gmail.com>
2025-05-19 16:49:52 +0530

    new code

--- app/services/servicesdashboard/page.jsx
+++ app/services/servicesdashboard/page.jsx
@@ -156,6 +156,7 @@ const portfolioItems = [
   },
 ];
 
+
 export default function Home() {
   const [selectedCategory, setSelectedCategory] = useState(portfolioItems[0]);
   const [fullViewImage, setFullViewImage] = useState(null);
