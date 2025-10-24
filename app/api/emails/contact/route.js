54e78e7697201ba2dc2fd68cb0901f6e7d892875
AkshayKalamkarr <kalamkarakshay2001@gmail.com>
2025-10-24 12:50:33 +0530

    New Code

--- app/api/emails/contact/route.js
+++ app/api/emails/contact/route.js
@@ -2,9 +2,6 @@ import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 
-
-
-
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request) {
