9e03dd067b61b4d61d08e7fff1b86dd11d8261bb
AkshayKalamkarr <kalamkarakshay2001@gmail.com>
2025-10-31 11:18:09 +0530

    New Code

--- data/residencialprojects.js
+++ data/residencialprojects.js
@@ -153,20 +153,16 @@ export const projects = [
     //     fullDescription:
     //         "This 2500 sq. ft. duplex flat is a seamless blend of traditional elegance and modern sophistication. The interiors open with a regal living room styled with Maharaja-inspired furniture, flowing into a sleek modular kitchen with smart storage and premium accessories. The master bedroom follows a modern theme, defined by clean lines and calming tones, while the floating ceiling with refined finishes and concealed lighting enhances spatial drama. Artistic embroidery-finished wallpapers add depth and luxury, complemented by a striking staircase design with mirror-accented panels. The outdoor space features a customized washbasin counter with invisible grills for safety without compromising the view, while the washroom is elevated with decor-highlighted floor tiles and a bespoke standalone basin. Every detail has been thoughtfully designed to create a home that feels both luxurious and timeless.",
     //     tagLine: "Project information",
-    //     reraNumber: "P52000011751",
+    //     reraNumber: "P52000011751",  
     //     highlights: [
-    //         { icon: "Sofa", description: "Magnetic Effect Ceiling" },
-    //         { icon: "ChefHat", description: "Modular kitchen with accessories" },
-    //         { icon: "Bed", description: "Modern theme master bedroom" },
-    //         { icon: "ChevronUp", description: "Floating ceiling with refined finish" },
-    //         { icon: "Badge", description: "Embroidery-finished highlighted wallpaper" },
-    //         { icon: "PanelsTopLeft", description: "Staircase with highlighted panel & mirror" },
-    //         { icon: "Shield", description: "Outdoor space with custom washbasin & invisible grill" },
-    //         { icon: "ShowerHead", description: "Washroom with decor floor tiles & standalone basin" },
-
+            // { icon: "Sofa", description: "Magnetic Effect Ceiling" },
+            // { icon: "Sofa", description: "Tropical Wallpaper Highlights"},
+            // { icon: "Sofa", description: "Customized Designer Hanging Light"},
+            // { icon: "Sofa", description: "Hidden Window With Moulded Panelling"},
+            // { icon: "Sofa", description: "Cane-Finished Storage With Modern Touch"}
     //     ],
     //     configuration: [
-    //         { flat: "2500 sqft", carpet: "60 Days" },
+    //         { flat: "550 sqft", carpet: "30 Days" },
     //     ],
 
     //     galleryImages: [
